3305b07cbedad296243f293b44a699b1cc0bdcbe
Temi-Pinheiro <temitopepinheiro@gmail.com>
2021-05-14 00:03:18 +0100

    come on brooo

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,18 @@ const Login = (props) => {
           <SignIn>Sign In</SignIn>
         </div>
       </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional communnity</h1>
+          <img src='/images/login-hero.svg' />
+        </Hero>
+        <Form>
+          <Google>
+            <img src='/images/google.svg' />
+            Sign in with Google
+          </Google>
+        </Form>
+      </Section>
     </Container>
   );
 };
@@ -51,11 +63,118 @@ const Join = styled.a`
     color: rgba(0, 0, 0, 0.9);
     text-decoration: none;
     border-radius: 4px;
+    cursor: pointer;
   }
 `;
 
 const SignIn = styled.a`
   box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition-duration: 167ms;
+  font-weight: 600;
+  font-size: 16px;
+  /* line-height: 40px; */
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    cursor: pointer;
+    color: #0a66c2;
+    text-decoration: none;
+  }
+`;
+
+const Section = styled.section`
+  align-content: flex-start;
+  display: flex;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+
+  @media (max-width: 768px;) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+
+  h1 {
+    padding-bottom: 0;
+    font-size: 56px;
+    width: 55%;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 25px;
+      font-weight: 500;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+
+  img {
+    /* z-index: -1; */
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
+    }
+  }
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 20px;
+    /* display: flex;
+    justify-content: center; */
+  }
+`;
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%), inset 0 0 0 2px rgb(0 0 0 / 0%),
+    inset 0 0 0 1px rgb(0 0 0 / 0);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
+    cursor: pointer;
+  }
 `;
 
 export default Login;
